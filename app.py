38450b4530e9f3c0c3b648505d9eae3a6221240f
waterkat <waterkat@aonyxlimited.com>
2024-09-21 05:28:40 +0000

    adding flask boiler plate for chess game app

--- app.py
+++ app.py
@@ -1,3 +1,22 @@
+from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chess.db'
+db = SQLAlchemy(app)
+
+class Game(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  content = db.Column(db.String(200), nullable=False)
+  date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+with app.app_context():
+  db.create_all()
+
+@app.route('/', methods=['GET'])
+def index():
+  return render_template('index.html')
+
 if __name__ == "__main__":
-    print("Hello World!")
-    
+  app.run(debug=True)
