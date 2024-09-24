0f834744da12e4ca251a19fb67961918421303d3
waterkat <waterkat@aonyxlimited.com>
2024-09-24 01:40:53 +0000

    adding display table for games

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -8,15 +8,35 @@ db = SQLAlchemy(app)
 
 class Game(db.Model):
   id = db.Column(db.Integer, primary_key=True)
+  black_id=db.Column(db.String(200), nullable=False, default='black')
+  white_id=db.Column(db.String(200), nullable=False, default='white')
   content = db.Column(db.String(200), nullable=False, default='rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR')
+  last_play_date = db.Column(db.DateTime, default=datetime.utcnow)
   date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
 with app.app_context():
   db.create_all()
 
+@app.route('/game', methods=['POST'])
+def create_game():
+  new_game = Game()
+  #new_game.black_id = random()
+  #new_game.white_id = random()
+  
+  try:
+    db.session.add(new_game)
+    db.session.commit()
+    
+  except:
+    return 'Something went wrong creating your game.'
+  
+  return redirect(f"/game/{new_game.white_id}")
+
+
 @app.route('/', methods=['GET'])
 def index():
-  return render_template('index.html')
+  active_games = Game.query.order_by(Game.last_play_date).all()
+  return render_template('index.html', active_games=active_games)
 
 if __name__ == "__main__":
   app.run(debug=True)
